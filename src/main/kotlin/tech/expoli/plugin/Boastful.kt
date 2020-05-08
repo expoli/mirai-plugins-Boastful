8b056626afc7a3a41150aba8e23beebabb01bbd5
expoli <me@expoli.tech>
2020-05-08 11:43:28 +0800

    初版开发完成，开始构建测试。

---
+++ src/main/kotlin/tech/expoli/plugin/Boastful.kt
@@ -0,0 +1,61 @@
+package tech.expoli.plugin
+
+import net.mamoe.mirai.console.plugins.ConfigSection
+import java.util.*
+
+class Boastful(lotsPath: String) {
+
+    private var lots: List<ConfigSection>?
+    private var lotMap = mutableMapOf<Long, Int>()
+    private var day: Int
+
+    /**
+     * 初始化，加载数据
+     */
+    init {
+        day = Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        try {
+            lots = BoastfulMain.getResourcesConfig(lotsPath).getConfigSectionList("Lots")
+            BoastfulMain.logger.info("本地夸夸其谈数据加载成功,数据大小为：" + lots!!.size)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            BoastfulMain.logger.info("无法加载本地夸夸其谈数据")
+            lots = null
+        }
+
+    }
+
+    /**
+     *  抽签
+     */
+    fun sign(id: Long): String {
+        return if (lots != null) {
+            checkDay()
+            if (lotMap.contains(id)) {
+                "^_^ 今天已经夸过了你了呦,明天再来吧! ^_^"
+            } else {
+                val index = lots!!.indices.random()
+                lotMap[id] = index
+                val lot = lots!![index]
+                "夸语第" + lot.getString("uid") + "\n" +
+                        lot.getString("sign") + "\n" +
+                        "^_^"
+            }
+        } else {
+            "Lots init failed!"
+        }
+    }
+
+
+    /**
+     * 检查当前时间，换了天数就把记录的Map清楚掉
+     */
+    private fun checkDay() {
+        val dayTemp = Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        if (dayTemp != day) {
+            day = dayTemp
+            lotMap.clear()
+        }
+    }
+}
+
